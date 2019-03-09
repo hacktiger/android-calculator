aaea90a58498eef4b05a11b7ae542a4fce2cc64f
ngthailam <ngthailam98@gmail.com>
2019-03-09 16:47:56 +0700

    fix constraint problems

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapVar();
+    }
+
+    private void mapVar() {
+        mTextView = findViewById(R.id.text_view);
+        mBtn = findViewById(R.id.btn_c);
+        mBtn.setOnClickListener(v -> {
+            mTextView.setText("AAAAA");
+        });
     }
 }
